b27a77e6667d1c89cd4173902a4e6c88c1c4419c
binhth.vuejx@gmail.com <binhth.vuejx@gmail.com>
2018-07-17 22:07:27 +0700

    commit
    commit

--- onegate_21_fe/src/components/Landing.vue
+++ onegate_21_fe/src/components/Landing.vue
@@ -2,7 +2,6 @@
   <div>
     <v-layout wrap class="menu_header_list" :class='{"no__border__bottom": btnDynamics === null || btnDynamics === undefined || btnDynamics === "undefined" || (btnDynamics !== null && btnDynamics !== undefined && btnDynamics !== "undefined" && btnDynamics.length === 0)}'>
       <v-flex xs12 class="px-2">
-        
         <v-select
           :items="listThuTucHanhChinh"
           v-model="thuTucHanhChinhSelected"
