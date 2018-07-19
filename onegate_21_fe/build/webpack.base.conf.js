800014447d2705e726421071477bcfca9613b61c
binhth.vuejx@gmail.com <binhth.vuejx@gmail.com>
2018-07-19 08:58:58 +0700

    commit
    commit

--- onegate_21_fe/build/webpack.base.conf.js
+++ onegate_21_fe/build/webpack.base.conf.js
@@ -9,7 +9,8 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './src/main.js',
+    app_dvc: './src/main_dvc.js'
   },
   output: {
     path: config.build.assetsRoot,
