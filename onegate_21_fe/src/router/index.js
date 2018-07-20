95796e573044733672bb0c3e4fe7278a3ea65ee0
hoang anh <hoanganh16091995@gmail.com>
2018-07-20 16:15:07 +0700

    commit to merge

--- onegate_21_fe/src/router/index.js
+++ onegate_21_fe/src/router/index.js
@@ -4,7 +4,7 @@ import Router from 'vue-router'
 const routerOptions = [
   { path: '/danh-sach-ho-so/:index', component: 'Landing', props: true },
   { path: '/tra-cuu-ho-so', component: 'TraCuuHoSo', props: true },
-  { path: '/danh-sach-ho-so/:index/tiep-nhan-ho-so/:id/:formCode', component: 'TiepNhanHoSo', props: true },
+  { path: '/danh-sach-ho-so/:index/tiep-nhan-ho-so/:id/:formCode', component: 'TiepNhanHoSoDetail', props: true },
   { path: '/danh-sach-ho-so/:index/chi-tiet-ho-so/:id', component: 'XemChiTietHoSoDetailCanBo', props: true },
   { path: '/danh-sach-ho-so/:index/bo-sung-ho-so/:id', component: 'HoanThienBoSungHoSoDetail', props: true },
   { path: '/danh-sach-ho-so/:index/tra-ket-qua/:id', component: 'TraKetQuaDetail', props: true },
