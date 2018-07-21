3c2d918539651eacc4527da125e15651183f3a87
hoang anh <hoanganh16091995@gmail.com>
2018-07-21 09:05:09 +0700

    commit to merge

--- onegate_21_fe/src/components/Landing.vue
+++ onegate_21_fe/src/components/Landing.vue
@@ -167,6 +167,7 @@
                   item-value="processOptionId"
                   return-object
                   :hide-selected="true"
+                  v-if="thuTucHanhChinhSelected"
                   :rules="[v => !!v || 'dịch vụ bắt buộc phải chọn.']"
                   @change = "changeDichVuConfigs"
                   required
@@ -666,21 +667,21 @@ export default {
       let currentQuery = router.history.current.query
       console.log('currentQuery', currentQuery)
       if (currentQuery.hasOwnProperty('q')) {
+        // let filter = {
+        //   queryParams: currentQuery.q,
+        //   page: vm.hosoDatasPage,
+        //   agency: vm.govAgencyCode,
+        //   service: vm.serviceCode,
+        //   template: vm.templateNo
+        // }
+        // /*  test Local */
         let filter = {
-          queryParams: currentQuery.q,
+          queryParams: 'http://127.0.0.1:8081' + currentQuery.q,
           page: vm.hosoDatasPage,
           agency: vm.govAgencyCode,
           service: vm.serviceCode,
           template: vm.templateNo
         }
-        // /*  test Local */
-        // // let filter = {
-        // //   queryParams: 'http://127.0.0.1:8081' + currentQuery.q,
-        // //   page: vm.hosoDatasPage,
-        // //   agency: vm.govAgencyCode,
-        // //   service: vm.serviceCode,
-        // //   template: vm.templateNo
-        // // }
         vm.$store.dispatch('loadingDataHoSo', filter).then(function (result) {
           vm.hosoDatas = result.data
           vm.hosoDatasTotal = result.total
