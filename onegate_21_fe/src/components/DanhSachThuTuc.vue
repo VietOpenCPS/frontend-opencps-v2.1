e2893aac70d05ee6304439164a0373571e50a8c8
hoang anh <hoanganh16091995@gmail.com>
2018-07-30 09:51:23 +0700

    update onegate_fe

--- onegate_21_fe/src/components/DanhSachThuTuc.vue
+++ onegate_21_fe/src/components/DanhSachThuTuc.vue
@@ -10,28 +10,25 @@
       <p class="mt-4">đang tạo hồ sơ ...</p>
     </div>
     <v-expansion-panel v-else expand class="expand__select__gov">
-      <v-expansion-panel-content v-for='(itemGov, index) in govAgencies' :key='index' value="1">
+      <v-expansion-panel-content v-for='(itemGov, index) in govAgencies' :key='index'>
         <div slot='header' class="ml-3"><v-icon>account_balance</v-icon> {{itemGov.govAgencyName}}</div>
         <v-card>
           <v-card-text class='grey lighten-3 px-0 py-0'>
             <!-- Cap 2 -->
             <v-expansion-panel expand class="expand__select__domain">
-              <v-expansion-panel-content v-for='(itemDomain, index2) in itemGov.domains' :key='index2' v-if='itemGov.domains' value="1">
+              <v-expansion-panel-content v-for='(itemDomain, index2) in itemGov.domains' :key='index2' v-if='itemGov.domains'>
                 <div slot='header' style="margin-left: 40px;">{{itemDomain.domainName}}</div>
                 <v-card>
                   <v-card-text class="card__text__gov" style="font-size: 13px; margin-left: 38px;">
                     <!-- Cap 3 -->
                     <div slot="header">
-                      <v-layout row wrap v-for='(itemServiceConfig, index3) in itemDomain.serviceConfigs' v-if='itemDomain.serviceConfigs' :key='index3'>
+                      <v-layout row wrap v-for='(itemServiceConfig, index3) in itemDomain.serviceConfigs' v-if='itemDomain.serviceConfigs' :key='index3' class="center-all ml-2 mb-2">
                         <v-flex xs12 sm9>
-                          <v-subheader style="font-size: 13px; float: left;">
-                            <span style="font-weight: bold">{{index3 + 1}}. </span>{{itemServiceConfig.serviceInfoName}}
-                          </v-subheader>
+                          <span style="font-weight: bold">{{index3 + 1}}.</span> &nbsp;
+                          <span>{{itemServiceConfig.serviceInfoName}}</span>
                         </v-flex>
-                        <v-flex xs12 sm1>
-                          <v-subheader style="font-size: 13px; float: left;">
-                            Mức {{itemServiceConfig.level}}
-                          </v-subheader>
+                        <v-flex xs12 sm1 class="text-xs-center">
+                          Mức {{itemServiceConfig.level}}
                         </v-flex>
                         <v-flex xs12 sm2 class="text-xs-center">
                           <v-menu left>
