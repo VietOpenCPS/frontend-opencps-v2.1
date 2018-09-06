3aa540833bf5b2890635abf5e72f50e4105b768e
binhth.vuejx@gmail.com <binhth.vuejx@gmail.com>
2018-09-06 13:47:29 +0700

    commit
    commit

--- onegate_21_fe/src/router/index.js
+++ onegate_21_fe/src/router/index.js
@@ -1,25 +1,26 @@
 import Vue from 'vue/dist/vue.min.js'
 import Router from 'vue-router'
+import Landing from '@/components/Landing'
+import TraCuuHoSo from '@/components/TraCuuHoSo'
+import TiepNhanHoSoDetail from '@/components/TiepNhanHoSoDetail'
+import XemChiTietHoSoDetailCanBo from '@/components/XemChiTietHoSoDetailCanBo'
+import XuLyHoSo from '@/components/XuLyHoSo'
+import HoanThienBoSungHoSoDetail from '@/components/HoanThienBoSungHoSoDetail'
+import DetailForward from '@/components/DetailForward'
+import DanhSachThuTuc from '@/components/DanhSachThuTuc'
+import NotFound from '@/components/NotFound'
 
-const routerOptions = [
-  { path: '/danh-sach-ho-so/:index', component: 'Landing', props: true },
-  { path: '/tra-cuu-ho-so', component: 'TraCuuHoSo', props: true },
-  { path: '/danh-sach-ho-so/:index/tiep-nhan-ho-so/:id/:formCode', component: 'TiepNhanHoSoDetail', props: true },
-  { path: '/danh-sach-ho-so/:index/chi-tiet-ho-so/:id', component: 'XemChiTietHoSoDetailCanBo', props: true },
-  { path: '/danh-sach-ho-so/:index/xu-ly-ho-so', component: 'XuLyHoSo', props: true },
-  { path: '/danh-sach-ho-so/:index/bo-sung-ho-so/:id', component: 'HoanThienBoSungHoSoDetail', props: true },
-  { path: '/danh-sach-ho-so/:index/tra-ket-qua/:id', component: 'TraKetQuaDetail', props: true },
-  { path: '/danh-sach-ho-so/:index/ho-so/:id/:formCode', component: 'DetailForward', props: true },
-  { path: '/add-dvc/:serviceCode', component: 'DanhSachThuTuc', props: true },
-  { path: '*', component: 'NotFound' }
+const routes = [
+  { path: '/danh-sach-ho-so/:index', name: 'Landing', component: Landing, props: true },
+  { path: '/tra-cuu-ho-so', name: 'TraCuuHoSo', component: TraCuuHoSo, props: true },
+  { path: '/danh-sach-ho-so/:index/tiep-nhan-ho-so/:id/:formCode', name: 'TiepNhanHoSoDetail', component: TiepNhanHoSoDetail, props: true },
+  { path: '/danh-sach-ho-so/:index/chi-tiet-ho-so/:id', name: 'XemChiTietHoSoDetailCanBo', component: XemChiTietHoSoDetailCanBo, props: true },
+  { path: '/danh-sach-ho-so/:index/xu-ly-ho-so', name: 'XuLyHoSo', component: XuLyHoSo, props: true },
+  { path: '/danh-sach-ho-so/:index/bo-sung-ho-so/:id', name: 'HoanThienBoSungHoSoDetail', component: HoanThienBoSungHoSoDetail, props: true },
+  { path: '/danh-sach-ho-so/:index/ho-so/:id/:formCode', name: 'DetailForward', component: DetailForward, props: true },
+  { path: '/add-dvc/:serviceCode', name: 'DanhSachThuTuc', component: DanhSachThuTuc, props: true },
+  { path: '*', name: 'NotFound', component: NotFound }
 ]
-
-const routes = routerOptions.map(route => {
-  return {
-    ...route,
-    component: () => import(`@/components/${route.component}.vue`)
-  }
-})
 
 Vue.use(Router)
 
