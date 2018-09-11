1a1e2829f063b8e1d614eba5922ab2fcf65b5da9
hoang anh <hoanganh16091995@gmail.com>
2018-09-11 14:57:38 +0700

    update voting

---
+++ onegate_21_voting/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+  import router from '@/router'
+  export default {
+    data: () => ({
+      danhSachBaoCao: [],
+      isCallBack: true
+    }),
+    computed: {
+    },
+    created () {
+    },
+    updated () {
+    },
+    watch: {
+    },
+    methods: {
+    }
+  }
+</script>
