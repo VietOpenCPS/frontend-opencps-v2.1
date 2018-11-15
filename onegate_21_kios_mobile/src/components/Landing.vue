54e76a4207aab52ca80e20ae1b04a20fa8c3fb1a
binhthgc <binhth.vuejx@gmail.com>
2018-11-15 11:11:11 +0700

    commit
    commit

--- onegate_21_kios_mobile/src/components/Landing.vue
+++ onegate_21_kios_mobile/src/components/Landing.vue
@@ -198,6 +198,14 @@
         vm.daTiepNhan = 0
         vm.daGiaiQuyet = 0
         vm.phanTram = 0
+        vm.$store.dispatch('getAgencyReportListsHomePage').then(function (result) {
+          if (result === null || result === undefined || result === 'undefined') {
+          } else {
+            vm.daTiepNhan = result['processCount']
+            vm.daGiaiQuyet = result['releaseCount']
+            vm.phanTram = result['ontimePercentage']
+          }
+        })
       },
       doProcessReport2 (data) {
         let vm = this
