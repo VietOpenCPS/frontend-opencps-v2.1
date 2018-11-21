79a03d008536856732648a2a55964023c1501bb8
binhthgc <binhth.vuejx@gmail.com>
2018-11-21 16:26:07 +0700

    commit
    commit

--- deliverable/src/store/DeliverableTypes.js
+++ deliverable/src/store/DeliverableTypes.js
@@ -3,6 +3,8 @@ const getDeliverableTypes = `
         getDeliverableTypes(start: -1, end: -1) {
             typeCode
             typeName
+            formScriptFileId
+            formReportFileId
             dataConfig
             tableConfig
         }
