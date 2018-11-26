56c4698fc9e221775eb459bd4527f7c082056a56
binhthgc <binhth.vuejx@gmail.com>
2018-11-26 13:02:07 +0700

    c

--- deliverable/src/components/ext/ViewPDF.vue
+++ deliverable/src/components/ext/ViewPDF.vue
@@ -1,37 +1,33 @@
 <template>
   <div>
-    <vue-pdf-viewer width="100%" height="100%" style="overflow: auto;min-height: 600px;" :url="url"></vue-pdf-viewer>
-    <iframe v-show="!dialogPDFLoading" id="pdfViewer" src="" type="application/pdf" width="100%" height="100%" style="overflow: auto;min-height: 600px;" frameborder="0"></iframe>
+    <vue-friendly-iframe v-if="!dialogPDFLoading" :src="pdfBlob"></vue-friendly-iframe>
   </div>
 </template>
 
 <script>
 
-  import VuePDFViewer from 'vue-instant-pdf-viewer'
+  import VueFriendlyIframe from 'vue-friendly-iframe'
 
   export default {
     props: ['id', 'datainput'],
     components: {
-      'vue-pdf-viewer': VuePDFViewer
+      'vue-friendly-iframe': VueFriendlyIframe
     },
     data() {
       return {
         data: {},
         dialogPDFLoading: true,
         isCallBack: true,
-        url: 'blob:http://localhost:8080/0a3f225d-dd33-4e7d-80c1-fca34e02ac74'
+        pdfBlob: null
       }
     },
     created() {
       var vm = this
       vm.$nextTick(function () {
-        if (vm.isCallBack) {
-          vm.isCallBack = false
+        setTimeout(() => {
           vm.data = vm.datainput
-          console.log('vm.datainput', vm.datainput)
-          console.log('vm.data', vm.data)
           vm.pullPDF()
-        }
+        }, 100)
       })
     },
     methods: {
@@ -41,7 +37,7 @@
         console.log('pullPDF', vm.data['fileEntryId'])
         vm.$store.dispatch('viewPDF', vm.data['fileEntryId']).then(function (result) {
           vm.dialogPDFLoading = false
-          document.getElementById('pdfViewer').src = result
+          vm.pdfBlob = result
         })
       }
     }
