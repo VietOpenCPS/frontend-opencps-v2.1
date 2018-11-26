56e863e9abc0220beb29adaf3fdbff3080d8fd07
binhthgc <binhth.vuejx@gmail.com>
2018-11-26 15:55:35 +0700

    c

--- deliverable/src/components/ext/ViewLogs.vue
+++ deliverable/src/components/ext/ViewLogs.vue
@@ -3,7 +3,7 @@
     <div class="list_history_style" v-for="(item, indexHistory) in logs" v-bind:key="indexHistory">
       <td class="px-2 pt-2 col-tien-trinh-2">{{indexHistory}}</td>
       <td class="text-xs-left px-2 py-2">
-        <p class="mb-1"><span>{{item.actionDate}}</span> - <b>{{item.author}}</b> : <span style="color: rgb(11, 114, 186);">{{support[item.deliverableAction]}}</span></p>
+        <p class="mb-1"><span>{{item.actionDate}}</span> - <b>{{item.author}}</b> : <span style="color: rgb(11, 114, 186);">{{support['state_' + item.deliverableAction]}}</span></p>
       </td>
     </div>
   </div>
