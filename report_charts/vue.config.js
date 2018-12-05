29e7e9ca2a7359af5a06a5427077f6ab303efdff
binhthgc <binhth.vuejx@gmail.com>
2018-12-05 14:20:34 +0700

    commit

---
+++ report_charts/vue.config.js
@@ -0,0 +1,66 @@
+
+/*
+module.exports = {
+  runtimeCompiler: true,
+  configureWebpack: {
+    output: {
+      libraryExport: 'default'
+    }
+  },
+  chainWebpack: config => {
+    config.module.rule('images').use('url-loader')
+      .loader('file-loader') // replaces the url-loader
+      .tap(options => Object.assign(options, {
+        name: 'images/register/[name].[ext]'
+      }))
+    config.module.rule('svg').use('file-loader')
+      .tap(options => Object.assign(options, {
+        name: 'images/register/[name].[ext]'
+      }))
+  },
+  css: {
+    extract: {
+      filename: 'css/register/[name].css',
+      chunkFilename: 'css/register/[name].css'
+    }
+  },
+  configureWebpack: {
+    output: {
+      filename: 'js/register/[name].js',
+      chunkFilename: 'js/register/[name].js'
+    }
+  }
+}
+
+*/
+module.exports = {
+    runtimeCompiler: true,
+    configureWebpack: {
+      output: {
+        libraryExport: 'default'
+      }
+    },
+    chainWebpack: config => {
+      config.module.rule('images').use('url-loader')
+        .loader('file-loader') // replaces the url-loader
+        .tap(options => Object.assign(options, {
+          name: '../../docs/o/opencps-frontend/report_charts_new/images/[name].[ext]'
+        }))
+      config.module.rule('svg').use('file-loader')
+        .tap(options => Object.assign(options, {
+          name: '../../docs/o/opencps-frontend/report_charts_new/images/[name].[ext]'
+        }))
+    },
+    css: {
+      extract: {
+        filename: '../../docs/o/opencps-frontend/report_charts_new/css/[name].css',
+        chunkFilename: '../../docs/o/opencps-frontend/report_charts_new/css/[name].css'
+      }
+    },
+    configureWebpack: {
+      output: {
+        filename: '../../docs/o/opencps-frontend/report_charts_new/js/[name].js',
+        chunkFilename: '../../docs/o/opencps-frontend/report_charts_new/js/[name].js'
+      }
+    }
+  }
