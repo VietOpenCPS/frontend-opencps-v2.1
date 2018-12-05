8ccc88194f9659d56c1ae003312bcce93de3ec23
binhthgc <binhth.vuejx@gmail.com>
2018-12-05 17:36:19 +0700

    c

--- report_charts/src/components/screens/Landing.vue
+++ report_charts/src/components/screens/Landing.vue
@@ -737,7 +737,7 @@ export default {
             }
           }
         },
-        colors: vm.chartOptionsBar.colors,
+        colors: ['#33b2df', '#546E7A', '#d4526e', '#13d8aa', '#A5978B', '#2b908f', '#f9a3a4', '#90ee7e', '#f48024'],
         dataLabels: {
           enabled: true,
           textAnchor: 'start',
