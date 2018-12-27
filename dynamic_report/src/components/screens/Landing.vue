46425c5c5afdc97650331dff19d30551cb7972bc
binhthgc <binhth.vuejx@gmail.com>
2018-12-27 13:40:40 +0700

    c

--- dynamic_report/src/components/screens/Landing.vue
+++ dynamic_report/src/components/screens/Landing.vue
@@ -716,10 +716,22 @@ export default {
             resultData = vm.sortByKey(resultData, sort)
           }
           if (subKey !== null && subKey !== undefined && subKey !== '') {
-            resultData = resultData.sort(function (a, b) {
-              return a[sumKey] - b[sumKey] || a[subKey] - b[subKey]
-            })
-            console.log('resultDataresultDataresultDataresultData: ', resultData)
+            let arraySubKey = {}
+            for (let key in resultData) {
+              if ((resultData[key][sumKey] !== '' && String(resultData[key][sumKey]) !== '0' && resultData[key][sumKey] !== undefined && resultData[key][sumKey] !== null) ||
+                (subKey !== null && subKey !== undefined && subKey !== '' && resultData[key][subKey] === '' && resultData[key][sumKey] !== '' && resultData[key][sumKey] !== '0')) {
+                if (arraySubKey[resultData[key]['domain']] !== undefined && arraySubKey[resultData[key]['domain']] !== null) {
+                  arraySubKey[resultData[key]['domain']].push(resultData[key])
+                } else {
+                  arraySubKey[resultData[key]['domain']] = []
+                  arraySubKey[resultData[key]['domain']].push(resultData[key])
+                }
+              }
+            }
+            for (let key in arraySubKey) {
+              console.log('arraySubKey key', key)
+              console.log('arraySubKey', arraySubKey[key])
+            }
           }
           for (let key in resultData) {
             if ((resultData[key][sumKey] !== '' && String(resultData[key][sumKey]) !== '0' && resultData[key][sumKey] !== undefined && resultData[key][sumKey] !== null) ||
