bdcaf4b8d6a7223c0a2f2f88ac3339ce8a1ee0b4
binhthgc <binhth.vuejx@gmail.com>
2019-01-02 12:24:54 +0700

    c

---
+++ dynamic_report/src/components/screens/CsvDownload.vue
@@ -0,0 +1,38 @@
+<template>
+    <a :download="downloadName" :href="downloadUrl" :disabled="this.data.length === 0">
+       <v-icon>receipt</v-icon> &nbsp; Export CSV
+    </a>
+</template>
+
+<script>
+import json2csv from 'json2csv';
+export default {
+    props: {
+        fields: undefined,
+        data: undefined,
+        downloadName: {
+            default: 'export.csv'
+        }
+    },
+    data: () => ({
+        dataToExport: ''
+    }),
+    created () {
+        var vm = this
+        vm.$nextTick(function () {
+            let rawHeader = json2csv({fields: this.fields}) + '\n'
+            let rawData = json2csv({data: this.data})
+            let lines = rawData.split('\n');
+            lines.splice(0,1);
+            let newtext = rawHeader + lines.join('\n');
+            vm.dataToExport = newtext
+            console.log('dataToExport', vm.dataToExport)
+        })
+    },
+    computed: {
+        downloadUrl() {
+            return this.data.length > 0 ? "data:text/csv," + encodeURIComponent(this.dataToExport) : 'javascript:void(0);';
+        }
+    }
+}
+</script>
