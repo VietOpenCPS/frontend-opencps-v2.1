b9d426dc74c181d847ddb64b23d511606a037cd2
binhthgc <binhth.vuejx@gmail.com>
2019-01-18 09:12:06 +0700

    c

--- onegate_21_fe/src/main.js
+++ onegate_21_fe/src/main.js
@@ -1,33 +1,13 @@
-import 'vuetify/dist/vuetify.min.css'
-import 'toastr/build/toastr.min.css'
-import './stylus/main.css'
-import './stylus/jquery-comments.css'
 import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App'
-import router from './router_dev'
-import { store } from './store'
-import VueContentPlaceholders from 'vue-content-placeholders'
-import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.min.css'
-import 'v-suggestions/dist/v-suggestions.css'
-import 'svg-pan-zoom/dist/svg-pan-zoom.min.js'
-import axios from 'axios'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
 
-axios.defaults.headers.common['Token'] = window.Liferay !== undefined ? window.Liferay.authToken : ''
-Vue.use(VueContentPlaceholders)
-Vue.config.productionTip = true
-
-Vue.mixin({
-  methods: {
-    getOriginality: function () {
-      return 3
-    }
-  }
-})
+Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
   router,
   store,
-  render: h => h(App)
-})
+  render: function (h) { return h(App) }
+}).$mount('#app')
