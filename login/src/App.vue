54c4df748af2f1f80e8b179cb4c8f53dd55f55e1
binhthgc <binhth.vuejx@gmail.com>
2019-01-19 22:23:59 +0700

    c

---
+++ login/src/App.vue
@@ -0,0 +1,202 @@
+<template>
+  <v-app id="app_login_hight_bee" class="onegate__fe">
+    sfas fs 
+  </v-app>
+</template>
+
+<script>
+  import { isMobile } from 'mobile-device-detect'
+  export default {
+    data: () => ({
+      isCallBack: true,
+      trangThaiHoSoList: [],
+      loading: true,
+      currentStep: '0',
+      counterData: [],
+      detailState: 0
+    }),
+    computed: {
+      currentIndex () {
+        return this.$store.getters.index
+      },
+      activeGetCounter () {
+        return this.$store.getters.activeGetCounter
+      },
+      viewMobile () {
+        return isMobile
+      },
+      pathLanding () {
+        return isMobile ? '/m/danh-sach-ho-so' : '/danh-sach-ho-so'
+      }
+    },
+    created () {
+      var vm = this
+      vm.$nextTick(function () {
+        vm.loading = true
+        vm.$store.dispatch('loadMenuConfigToDo').then(function (result) {
+          vm.loading = false
+          if (result) {
+            vm.trangThaiHoSoList = result
+            let currentParams = vm.$router.history.current.params
+            if (Array.isArray(vm.trangThaiHoSoList) && vm.trangThaiHoSoList.length > 0) {
+              if (!currentParams.hasOwnProperty('index') && !currentParams.hasOwnProperty('serviceCode')) {
+                vm.trangThaiHoSoList[0]['active'] = true
+               vm.$router.push({
+                  path: vm.pathLanding + '/0',
+                  query: {
+                    q: vm.trangThaiHoSoList[0]['queryParams']
+                  }
+                })
+              } else {
+                vm.trangThaiHoSoList[currentParams.index]['active'] = true
+              }
+            }
+            vm.loadingCounter()
+            vm.loading = false
+          }
+        })
+      })
+    },
+    updated () {
+      var vm = this
+      vm.$nextTick(function () {
+        let currentParams = vm.$router.history.current.params
+        if (currentParams.hasOwnProperty('index') && vm.isCallBack) {
+          vm.isCallBack = false
+          vm.$store.commit('setIndex', currentParams.index)
+          if (currentParams.hasOwnProperty('id')) {
+            vm.detailState = 1
+          } else {
+            vm.detailState = 0
+          }
+        }
+      })
+    },
+    watch: {
+      '$route': function (newRoute, oldRoute) {
+        let vm = this
+        let currentParams = newRoute.params
+        let currentQuery = newRoute.query
+        if (currentQuery.hasOwnProperty('step')) {
+          vm.currentStep = String(currentQuery.step)
+          console.log('currentStep', String(currentQuery.step))
+        } else {
+          vm.currentStep = '0'
+        }
+        if (currentQuery.hasOwnProperty('recount')) {
+          vm.loadingCounter()
+        }
+        if (currentParams.hasOwnProperty('id')) {
+          vm.detailState = 1
+        } else {
+          vm.detailState = 0
+        }
+      },
+      activeGetCounter (val) {
+        var vm = this
+        setTimeout(function () {
+          vm.loadingCounter()
+        }, 300)
+      }
+    },
+    methods: {
+      toTableIndexing (item, index) {
+        let vm = this
+        this.$store.commit('setIndex', index)
+       vm.$router.push({
+          path: vm.pathLanding + '/' + index,
+          query: {
+            renew: Math.floor(Math.random() * (100 - 1 + 1)) + 1,
+            q: item.queryParams
+          }
+        })
+      },
+      filterSteps (item) {
+        let vm = this
+        let currentQuery = vm.$router.history.current.query
+        let currentParams = vm.$router.history.current.params
+        console.log('currentParams', currentParams)
+        console.log('currentQuery', currentQuery)
+        let queryString = vm.trangThaiHoSoList[currentParams.index].queryParams
+        console.log('queryString++++++1111111+++++', queryString)
+        /* test Local */
+        /* let queryString = this.trangThaiHoSoList[0].queryParams */
+        if (currentQuery !== null && currentQuery !== undefined) {
+          let coma = queryString.lastIndexOf('=')
+          if (coma > 0) {
+            queryString = queryString.substr(0, coma + 1)
+          }
+        }
+        vm.currentStep = String(item.stepCode)
+        // if (vm.currentStep) {
+        //   let urls = queryString.split("?")
+        //   if (urls !== null && urls.length !== 0) {
+        //     for (var i = 0; i < urls.length; i++) {
+        //       if (urls[i].indexOf('step') > 0) {
+        //         urls.splice(i, 1)
+        //       }
+        //     }
+        //   }
+        // }
+        console.log('queryString++++++++22222222+++', queryString)
+       vm.$router.push({
+          path: vm.pathLanding + '/' + currentParams.index,
+          query: {
+            renew: Math.floor(Math.random() * (100 - 1 + 1)) + 1,
+            q: queryString + item.stepCode,
+            step: item.stepCode
+          }
+        })
+      },
+      loadingCounter () {
+        let vm = this
+        let filter = {
+          originality: vm.getOriginality()
+        }
+        vm.$store.dispatch('loadingCounterHoSo', filter).then(function (result) {
+          vm.counterData = result.data
+          for (let key in vm.trangThaiHoSoList) {
+            vm.trangThaiHoSoList[key]['counter'] = 0
+            if (vm.trangThaiHoSoList[key].hasOwnProperty('items')) {
+              let parentCount = 0
+              for (let keyChild in vm.trangThaiHoSoList[key].items) {
+                vm.trangThaiHoSoList[key].items[keyChild]['counter'] = 0
+                for (let countKey in vm.counterData) {
+                  if (String(vm.counterData[countKey].stepCode) === String(vm.trangThaiHoSoList[key].items[keyChild].stepCode)) {
+                    let countCurrent = vm.counterData[countKey].totalCount
+                    vm.trangThaiHoSoList[key].items[keyChild]['counter'] = countCurrent
+                    parentCount = parentCount + countCurrent
+                    break
+                  }
+                }
+              }
+              vm.trangThaiHoSoList[key]['counter'] = parentCount
+            } else {
+              if (vm.trangThaiHoSoList[key].queryParams.indexOf('step') >= 0) {
+                let stepParent = vm.trangThaiHoSoList[key].queryParams.split('step=')
+                let countParent = 0
+                for (let countKey in vm.counterData) {
+                  if (String(vm.counterData[countKey].stepCode) === String(stepParent[1])) {
+                    let countParent = vm.counterData[countKey].totalCount
+                    break
+                  }
+                }
+                vm.trangThaiHoSoList[key]['counter'] = countParent
+              } else {
+                let filter = {
+                  queryParams: vm.trangThaiHoSoList[key].queryParams
+                }
+                vm.$store.dispatch('loadingCounterNotStep', filter).then(function (result) {
+                  vm.trangThaiHoSoList[key]['counter'] = result.total
+                }).catch(function () {
+                  vm.trangThaiHoSoList[key]['counter'] = 0
+                })
+              }
+            }
+          }
+          vm.loading = false
+        })
+      }
+    }
+  }
+</script>
