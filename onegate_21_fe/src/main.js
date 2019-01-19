e637d60f4ac053f1b0d32b50b94cebd17845edaf
binhthgc <binhth.vuejx@gmail.com>
2019-01-19 10:34:55 +0700

    c

--- onegate_21_fe/src/main.js
+++ onegate_21_fe/src/main.js
@@ -4,8 +4,16 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router_dev'
 import { store } from './store'
+import VueContentPlaceholders from 'vue-content-placeholders'
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.min.css'
+import 'v-suggestions/dist/v-suggestions.css'
+import 'svg-pan-zoom/dist/svg-pan-zoom.min.js'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+axios.defaults.headers.common['Token'] = window.Liferay !== undefined ? window.Liferay.authToken : ''
+Vue.use(VueContentPlaceholders)
+Vue.use(Vuetify)
+Vue.config.productionTip = true
 
 Vue.mixin({
   methods: {
