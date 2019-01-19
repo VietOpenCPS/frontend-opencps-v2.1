a1be6f78666623be0760ab0a7d977085004be149
binhthgc <binhth.vuejx@gmail.com>
2019-01-19 13:03:33 +0700

    c

--- onegate_21_fe/src/components/XemChiTietHoSoDetailCanBo.vue
+++ onegate_21_fe/src/components/XemChiTietHoSoDetailCanBo.vue
@@ -1,17 +1,5 @@
 <template>
   <div class="form-chitiet">
-    <v-expansion-panel
-        :value="[true]"
-        expand
-      >
-        <v-expansion-panel-content
-        >
-          <div slot="header">Item</div>
-          <v-card>
-            <v-card-text class="grey lighten-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
-          </v-card>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
     <content-placeholders class="mt-3" v-if="loading">
       <content-placeholders-text :lines="1" />
     </content-placeholders>
