af323f1448c4206d084cd325e003d52bf783a2f5
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 16:44:55 +0700

    c

--- dynamic_report/src/components/Landing.vue
+++ dynamic_report/src/components/Landing.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    uuuuuuu
+    uuuuuuu222
     <v-layout wrap>
       <div>
         <v-chip v-for="(item, index) in dossierCounting" v-bind:key="index">
--- dynamic_report/src/router_dev/index.js
+++ dynamic_report/src/router_dev/index.js
@@ -3,12 +3,20 @@ import Router from 'vue-router'
 import Landing from '@/components/Landing'
 import TraCuuHoSo from '@/components/TraCuuHoSo'
 import TiepNhanHoSoDetail from '@/components/TiepNhanHoSoDetail'
+import XemChiTietHoSoDetailCanBo from '@/components/XemChiTietHoSoDetailCanBo'
+import XuLyHoSo from '@/components/XuLyHoSo'
+import HoanThienBoSungHoSoDetail from '@/components/HoanThienBoSungHoSoDetail'
+import DetailForward from '@/components/DetailForward'
 import NotFound from '@/components/NotFound'
 
 const routes = [
   { path: '/danh-sach-ho-so/:index', name: 'Landing', component: Landing, props: true },
   { path: '/tra-cuu-ho-so', name: 'TraCuuHoSo', component: TraCuuHoSo, props: true },
   { path: '/danh-sach-ho-so/:index/tiep-nhan-ho-so/:id/:formCode', name: 'TiepNhanHoSoDetail', component: TiepNhanHoSoDetail, props: true },
+  { path: '/danh-sach-ho-so/:index/chi-tiet-ho-so/:id', name: 'XemChiTietHoSoDetailCanBo', component: XemChiTietHoSoDetailCanBo, props: true },
+  { path: '/danh-sach-ho-so/:index/xu-ly-ho-so', name: 'XuLyHoSo', component: XuLyHoSo, props: true },
+  { path: '/danh-sach-ho-so/:index/bo-sung-ho-so/:id', name: 'HoanThienBoSungHoSoDetail', component: HoanThienBoSungHoSoDetail, props: true },
+  { path: '/danh-sach-ho-so/:index/ho-so/:id/:formCode', name: 'DetailForward', component: DetailForward, props: true },
   { path: '*', name: 'NotFound', component: NotFound }
 ]
 
