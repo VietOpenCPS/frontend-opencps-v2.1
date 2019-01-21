89160da854840562ac2c42c83d2942b266a04101
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 16:56:31 +0700

    c

--- dynamic_report/src/components/Landing.vue
+++ dynamic_report/src/components/Landing.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    uuuuuuu222rẻwer111fff
+    uuuuuuu222rẻwer111fffxxxx
     <v-layout wrap>
       <div>
         <v-chip v-for="(item, index) in dossierCounting" v-bind:key="index">
