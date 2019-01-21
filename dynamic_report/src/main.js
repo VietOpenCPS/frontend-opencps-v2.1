e8ce6cfbdcc4843ccd2198cef27acf2c1fd4eaf6
binhthgc <binhth.vuejx@gmail.com>
2019-01-22 03:21:55 +0700

    c

--- dynamic_report/src/main.js
+++ dynamic_report/src/main.js
@@ -1,44 +1,23 @@
 import 'idempotent-babel-polyfill'
 import Vue from 'vue'
-import App from './App'
+import './plugins/vuetify'
+import App from './App.vue'
 import router from './router_dev'
 import { store } from './store'
 import VueContentPlaceholders from 'vue-content-placeholders'
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.min.css'
+import 'v-suggestions/dist/v-suggestions.css'
+import 'svg-pan-zoom/dist/svg-pan-zoom.min.js'
 import axios from 'axios'
-import './plugins/vuetify'
 
-Vue.use(VueContentPlaceholders)
-
-let groupId = window.themeDisplay !== undefined ? window.themeDisplay.getScopeGroupId() : 0
-
-axios.defaults.withCredentials = true
 axios.defaults.headers.common['Token'] = window.Liferay !== undefined ? window.Liferay.authToken : ''
-axios.defaults.headers.common['groupId'] = groupId
-
+Vue.use(VueContentPlaceholders)
 Vue.config.productionTip = true
 
 Vue.mixin({
   methods: {
-    getScopeGroupId: function () {
-      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
-        return window.themeDisplay.getScopeGroupId()
-      } else {
-        return 0
-      }
-    },
-    getAuthToken: function () {
-      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
-        return window.Liferay.authToken
-      } else {
-        return ''
-      }
-    },
-    getUserId: function () {
-      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
-        return window.themeDisplay.getUserId()
-      } else {
-        return 0
-      }
+    getOriginality: function () {
+      return 3
     }
   }
 })
@@ -46,5 +25,9 @@ Vue.mixin({
 new Vue({
   router,
   store,
+  beforeCreate () {
+    var vm = this
+    vm.$store.dispatch('loadInitResource')
+  },
   render: function (h) { return h(App) }
 }).$mount('#app_dynamic_report')
