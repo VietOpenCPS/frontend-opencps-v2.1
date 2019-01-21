14d1ac528934df251d7e97eeeb70a892ca81e365
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 13:58:44 +0700

    c

--- dynamic_report/src/App.vue
+++ dynamic_report/src/App.vue
@@ -30,9 +30,61 @@
     </v-navigation-drawer>
     
     <div v-if="!isSignedIn">
-              123123 17777 123 2132131 adsfdsfsdfa 111
+              123123 17777 123 2132131 adsfdsfsdfa 9999
     </div>
     <div v-else>
+  
+      <div class="login_wrap_app_panel" style="text-align: right;max-width: 1366px;">
+        <v-badge color="red" v-if="notificationCount > 0">
+          <span slot="badge">2</span>
+          <v-btn icon class="mx-0 my-0">
+            <v-icon size="20" color="blue darken-3" class="swing animated" style="-webkit-animation: swing 0.8s infinite;animation: swing 0.8s infinite;">
+              notifications_active
+            </v-icon>
+          </v-btn>
+        </v-badge>
+        <v-btn v-else icon class="mx-0 my-0">
+          <v-icon size="20" color="blue darken-3" class="swing animated" style="-webkit-animation: swing 0.8s infinite;animation: swing 0.8s infinite;">
+            notifications
+          </v-icon>
+        </v-btn>
+        <v-menu offset-y :nudge-bottom="5">
+          <v-chip slot="activator">
+            <v-avatar v-if="avatarURL !== ''">
+              <img :src="avatarURL">
+            </v-avatar>
+            <v-avatar v-else class="white--text" :style="{background: '#' + colorBG}">
+              <span class="white--text">{{ userNameLogin.slice(0, 1).toUpperCase() }}</span>
+            </v-avatar>
+            {{userNameLogin}}
+            <v-icon v-if="!isShowUserMenu" size="20" color="blue darken-3" class="swing animated">
+              expand_more
+            </v-icon>
+            <v-icon v-else size="20" color="blue darken-3" class="swing animated">
+              expand_less
+            </v-icon>
+          </v-chip>
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon size="16">person</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>Thông tin tài khoản</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon size="16" color="red darken-3">exit_to_app</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>Đăng xuất</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+      </div>
+  
       
     </div>
   </v-app>
