40c3297d054e1f6b28d312676f90d7ac9a816bc8
binhthgc <binhth.vuejx@gmail.com>
2019-01-22 01:26:26 +0700

    c

--- dynamic_report/src/components/blacklist/Landing.vue
+++ dynamic_report/src/components/blacklist/Landing.vue
@@ -1,23 +1,12 @@
 <template>
   <div>
-   lllllllllllllllllllllllllll
-    <v-layout wrap>
-      <div>
-        <v-chip v-for="(item, index) in dossierCounting" v-bind:key="index">
-          <v-avatar v-if="item.key === 'deleted'"><v-icon size="16">delete</v-icon></v-avatar>
-          <v-avatar v-else>{{item.count}}</v-avatar>
-          {{item.title}}
-        </v-chip>
-      </div>
-    </v-layout>
+   123123123
   </div>
 </template>
 
 <script>
 export default {
-  props: ['index'],
   data: () => ({
-    dossierCounting: []
   })
 }
 </script>
