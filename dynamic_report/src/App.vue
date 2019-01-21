549fb771c1db4d58896e49fc356bc5a644b5ae23
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 13:39:17 +0700

    c

--- dynamic_report/src/App.vue
+++ dynamic_report/src/App.vue
@@ -9,8 +9,16 @@
     <div v-else>
   
       <div class="login_wrap_app_panel" style="text-align: right;max-width: 1366px;">
-        <v-btn icon class="mx-0 my-0">
-          <v-icon size="20" color="blue darken-3">
+        <v-badge color="red" v-if="notificationCount > 0">
+          <span slot="badge">2</span>
+          <v-btn icon class="mx-0 my-0">
+            <v-icon size="20" color="blue darken-3" class="swing animated" style="-webkit-animation: swing 0.8s infinite;animation: swing 0.8s infinite;">
+              notifications_active
+            </v-icon>
+          </v-btn>
+        </v-badge>
+        <v-btn v-else icon class="mx-0 my-0">
+          <v-icon size="20" color="blue darken-3" class="swing animated" style="-webkit-animation: swing 0.8s infinite;animation: swing 0.8s infinite;">
             notifications
           </v-icon>
         </v-btn>
