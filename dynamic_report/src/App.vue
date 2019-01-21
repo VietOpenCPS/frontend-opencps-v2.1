1bca1f37d0bf9dd74a6b1fa8c1ba2693853ddc5b
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 14:27:38 +0700

    c

--- dynamic_report/src/App.vue
+++ dynamic_report/src/App.vue
@@ -2,11 +2,11 @@
   <v-app id="app_dynamic_report" class="onegate__fe">
     <v-navigation-drawer app clipped floating width="240"
     >
-      1231231231233123
+      1231231231233123 qr r 
     </v-navigation-drawer>
     <v-content>
       <router-view></router-view>
-      123
+      123 we rwer
     </v-content>
   </v-app>
 </template>
