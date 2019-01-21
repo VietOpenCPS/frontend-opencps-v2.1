305f8d0b6e30f7d449765b8e532281ab8873aaca
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 16:53:59 +0700

    c

--- dynamic_report/src/router_dev/index.js
+++ dynamic_report/src/router_dev/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Landing from '@/components/Landing'
 import TiepNhanHoSoDetail from '@/components/TiepNhanHoSoDetail'
-import XemChiTietHoSoDetailCanBo from '@/components/XemChiTietHoSoDetailCanBo'
 import XuLyHoSo from '@/components/XuLyHoSo'
 import HoanThienBoSungHoSoDetail from '@/components/HoanThienBoSungHoSoDetail'
 import DetailForward from '@/components/DetailForward'
@@ -11,7 +10,6 @@ import NotFound from '@/components/NotFound'
 const routes = [
   { path: '/danh-sach-ho-so/:index', name: 'Landing', component: Landing, props: true },
   { path: '/danh-sach-ho-so/:index/tiep-nhan-ho-so/:id/:formCode', name: 'TiepNhanHoSoDetail', component: TiepNhanHoSoDetail, props: true },
-  { path: '/danh-sach-ho-so/:index/chi-tiet-ho-so/:id', name: 'XemChiTietHoSoDetailCanBo', component: XemChiTietHoSoDetailCanBo, props: true },
   { path: '/danh-sach-ho-so/:index/xu-ly-ho-so', name: 'XuLyHoSo', component: XuLyHoSo, props: true },
   { path: '/danh-sach-ho-so/:index/bo-sung-ho-so/:id', name: 'HoanThienBoSungHoSoDetail', component: HoanThienBoSungHoSoDetail, props: true },
   { path: '/danh-sach-ho-so/:index/ho-so/:id/:formCode', name: 'DetailForward', component: DetailForward, props: true },
