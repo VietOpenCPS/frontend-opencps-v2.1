ecd7f91a1557ea9e08ba730f8995da8101e68f8a
binhthgc <binhth.vuejx@gmail.com>
2019-01-22 00:30:17 +0700

    c

--- dynamic_report/src/components/XemChiTietHoSoDetailCanBo.vue
+++ dynamic_report/src/components/XemChiTietHoSoDetailCanBo.vue
@@ -23,34 +23,6 @@
                 </v-expansion-panel-content>
               </v-expansion-panel>
             </div>
-            <!-- Action button -->
-            <div class="px-4 py-3" v-if="btnStateVisible" style="border-bottom: 1px solid #dddddd;">
-              <v-btn color="primary" class="ml-0 mr-2" :class='{"deactive__btn": String(btnIndex) !== String(index)}' v-for="(item, index) in btnDossierDynamics" v-bind:key="index" 
-                v-on:click.native="processPullBtnDetail(item, index)" 
-                :loading="loadingAction && index === btnIndex"
-                :disabled="loadingAction || item.enable === 2"
-                v-if="item.enable > 0 || (item['autoEvent'] === 'special' && thongTinChiTietHoSo['permission'].indexOf('write') >= 0)"
-              >
-                {{item.actionName}}
-                <span slot="loader">Loading...</span>
-              </v-btn>
-              <v-btn color="primary" class="ml-0 mr-2" v-for="(item, index) in btnPlugins" v-bind:key="index" 
-                v-on:click.native="processPullBtnplugin(item, index)" 
-                :loading="loadingPlugin"
-                :disabled="loadingPlugin"
-              >
-                {{item.pluginName}}
-                <span slot="loader">Loading...</span>
-              </v-btn>
-              <v-menu bottom offset-y v-if="btnStepsDynamics.length > 0 && thongTinChiTietHoSo['permission'].indexOf('write') >= 0" style="display: inline-block;position:relative !important">
-                <v-btn slot="activator" class="deactive__btn" color="primary" dark>Khác &nbsp; <v-icon size="18">arrow_drop_down</v-icon></v-btn>
-                <v-list>
-                  <v-list-tile v-for="(item, index) in btnStepsDynamics" :key="index" @click="btnActionEvent(item, index)">
-                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                  </v-list-tile>
-                </v-list>
-              </v-menu>
-            </div>
           </v-tab-item>
         </v-tabs-items>
       </v-tabs>
