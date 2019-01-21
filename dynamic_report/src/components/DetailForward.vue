4bb5da3b70394ad3b666253164f9b5d608415899
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 17:55:35 +0700

    c

--- dynamic_report/src/components/DetailForward.vue
+++ dynamic_report/src/components/DetailForward.vue
@@ -15,10 +15,6 @@ import HoanThienBoSungHoSoDetail from './HoanThienBoSungHoSoDetail.vue'
 import HoSoLienThongDetail from './HoSoLienThongDetail.vue'
 // import HoSoLienThongTinhTrangXuLyDetail from './HoSoLienThongTinhTrangXuLyDetail.vue'
 import TiepNhanHoSoDetail from './TiepNhanHoSoDetail.vue'
-// import TiepNhanHoSoTrucTuyenDetail from './TiepNhanHoSoTrucTuyenDetail.vue'
-// import TraKetQuaDetail from './TraKetQuaDetail.vue'
-// import XemChiTietHoSoDetail from './XemChiTietHoSoDetail.vue'
-import XemChiTietHoSoDetailCanBo from './XemChiTietHoSoDetailCanBo.vue'
 
 export default {
   props: ['id', 'formCode'],
@@ -28,8 +24,6 @@ export default {
     'ho-so-lien-thong-detail': HoSoLienThongDetail,
     // 'ho-so-lien-thong-tinh-trang-xu-ly-detail': HoSoLienThongTinhTrangXuLyDetail,
     'tiep-nhan-ho-so-detail': TiepNhanHoSoDetail,
-    'xem-chi-tiet-ho-so-detail-can-bo': XemChiTietHoSoDetailCanBo,
-    // 'chi-tiet-tinh-trang-lien-thong': HoSoLienThongTinhTrangXuLyDetail,
     'chi-tiet-ho-so-lien-thong': HoSoLienThongDetail
   },
   data: () => ({
