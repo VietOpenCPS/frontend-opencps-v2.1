d97c10adbd394efb8fe68e10f5f51658911c10e3
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 14:55:15 +0700

    c

--- dynamic_report/src/main.js
+++ dynamic_report/src/main.js
@@ -10,14 +10,38 @@ import 'v-suggestions/dist/v-suggestions.css'
 import 'svg-pan-zoom/dist/svg-pan-zoom.min.js'
 import axios from 'axios'
 
-axios.defaults.headers.common['Token'] = window.Liferay !== undefined ? window.Liferay.authToken : ''
 Vue.use(VueContentPlaceholders)
+
+let groupId = window.themeDisplay !== undefined ? window.themeDisplay.getScopeGroupId() : 0
+
+axios.defaults.withCredentials = true
+axios.defaults.headers.common['Token'] = window.Liferay !== undefined ? window.Liferay.authToken : ''
+axios.defaults.headers.common['groupId'] = groupId
+
 Vue.config.productionTip = true
 
 Vue.mixin({
   methods: {
-    getOriginality: function () {
-      return 3
+    getScopeGroupId: function () {
+      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
+        return window.themeDisplay.getScopeGroupId()
+      } else {
+        return 0
+      }
+    },
+    getAuthToken: function () {
+      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
+        return window.Liferay.authToken
+      } else {
+        return ''
+      }
+    },
+    getUserId: function () {
+      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
+        return window.themeDisplay.getUserId()
+      } else {
+        return 0
+      }
     }
   }
 })
@@ -28,6 +52,18 @@ new Vue({
   beforeCreate () {
     var vm = this
     vm.$store.dispatch('loadInitResource')
+  },
+  created() {
+    var vm = this
+    vm.$nextTick(function() {
+      if (window.location.href.endsWith('#/')) {
+        let strQuery = ''
+        if (window.location.href.includes('/so-theo-doi')) {
+          strQuery = '?reportType=register'
+        }
+        vm.$router.push('/bao-cao/0' + strQuery)
+      }
+    })
   },
   render: function (h) { return h(App) }
 }).$mount('#app_dynamic_report')
