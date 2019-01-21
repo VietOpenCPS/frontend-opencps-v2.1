f9955cff4a199db5d5493688ec9eb93f51604ea0
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 23:47:29 +0700

    c

--- dynamic_report/src/components/XemChiTietHoSoDetailCanBo.vue
+++ dynamic_report/src/components/XemChiTietHoSoDetailCanBo.vue
@@ -68,67 +68,6 @@
                   </v-flex>
                 </v-layout>
               </div>
-              <div class="px-2 py-2" style="border: 1px solid #4caf50" v-if="thongTinChiTietHoSo.finishDate && btnStateVisible">
-                <p class="mb-2">
-                  Hồ sơ đã hoàn thành quá trình xử lý
-                </p>
-              </div>
-            </div>
-            <div style="position: relative;" v-if="checkInput !== 0 && filterNextActionEnable(btnDossierDynamics)">
-              <v-expansion-panel :value="[true]" class="expansion-pl">
-                <v-expansion-panel-content hide-actions :key="1">
-                  <div slot="header">
-                    <div class="background-triangle-small"> <v-icon size="18" color="white">star_rate</v-icon></div>
-                    <span v-if="checkInput === 2">Chỉnh sửa thành phần hồ sơ</span> 
-                    <span v-else>Kiểm tra thành phần hồ sơ</span>&nbsp;&nbsp;&nbsp;&nbsp; 
-                  </div>
-                  <thanh-phan-ho-so ref="thanhphanhoso" :checkInput="checkInput" :onlyView="false" :id="'ci'" :partTypes="inputTypes"></thanh-phan-ho-so>
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </div>
-            <!-- Action button -->
-            <div class="px-4 py-3" v-if="btnStateVisible" style="border-bottom: 1px solid #dddddd;">
-              <v-btn color="primary" class="ml-0 mr-2" :class='{"deactive__btn": String(btnIndex) !== String(index)}' v-for="(item, index) in btnDossierDynamics" v-bind:key="index" 
-                v-on:click.native="processPullBtnDetail(item, index)" 
-                :loading="loadingAction && index === btnIndex"
-                :disabled="loadingAction || item.enable === 2"
-                v-if="item.enable > 0 || (item['autoEvent'] === 'special' && thongTinChiTietHoSo['permission'].indexOf('write') >= 0)"
-              >
-                {{item.actionName}}
-                <span slot="loader">Loading...</span>
-              </v-btn>
-              <v-btn color="primary" class="ml-0 mr-2" v-for="(item, index) in btnPlugins" v-bind:key="index" 
-                v-on:click.native="processPullBtnplugin(item, index)" 
-                :loading="loadingPlugin"
-                :disabled="loadingPlugin"
-              >
-                {{item.pluginName}}
-                <span slot="loader">Loading...</span>
-              </v-btn>
-              <!-- Action rollBack -->
-              <!-- <v-btn color="primary" class="ml-0 mr-2 deactive__btn" v-if="String(currentUser.userId) === String(thongTinChiTietHoSo.lastActionUserId)"
-                @click="rollBackDossier(true)"
-                :loading="loadingAction"
-                :disabled="loadingAction"
-              >
-                Quay lại bước trước
-                <span slot="loader">Loading...</span>
-              </v-btn> -->
-              <!-- Action special -->
-              <v-menu bottom offset-y v-if="btnStepsDynamics.length > 0 && thongTinChiTietHoSo['permission'].indexOf('write') >= 0" style="display: inline-block;position:relative !important">
-                <v-btn slot="activator" class="deactive__btn" color="primary" dark>Khác &nbsp; <v-icon size="18">arrow_drop_down</v-icon></v-btn>
-                <v-list>
-                  <v-list-tile v-for="(item, index) in btnStepsDynamics" :key="index" @click="btnActionEvent(item, index)">
-                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                  </v-list-tile>
-                  <!-- <v-list-tile v-for="(item, index) in btnDossierDynamics" :key="index" 
-                    @click="processPullBtnDetail(item, index)" 
-                    v-if="checkPemissionSpecialAction(null, currentUser, thongTinChiTietHoSo)"
-                    >
-                    <v-list-tile-title>{{ item.actionName }}</v-list-tile-title>
-                  </v-list-tile> -->
-                </v-list>
-              </v-menu>
             </div>
           </v-tab-item>
           <v-tab-item value="tabs-2" :key="2" reverse-transition="fade-transition" transition="fade-transition">
