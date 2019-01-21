ea5e9f8f519d5185f4e6bbc00c495b2708744337
binhthgc <binhth.vuejx@gmail.com>
2019-01-22 03:48:32 +0700

    c

---
+++ dynamic_report/src/components/screens/Landing.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+   2222222333333333333333333333333333333333333
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  })
+}
+</script>
--- dynamic_report/src/router_dev/index.js
+++ dynamic_report/src/router_dev/index.js
@@ -5,8 +5,10 @@ import TiepNhanHoSoDetail from '@/components/blacklist/TiepNhanHoSoDetail'
 import HoanThienBoSungHoSoDetail from '@/components/blacklist/HoanThienBoSungHoSoDetail'
 import DetailForward from '@/components/blacklist/DetailForward'
 import NotFound from '@/components/NotFound'
+import LandingReport from '@/components/screens/Landing'
 
 const routes = [
+  { path: '/bao-cao/:index', name: 'LandingReport', component: LandingReport, props: true },
   { path: '/danh-sach-ho-so/:index', name: 'Landing', component: Landing, props: true },
   { path: '/danh-sach-ho-so/:index/tiep-nhan-ho-so/:id/:formCode', name: 'TiepNhanHoSoDetail', component: TiepNhanHoSoDetail, props: true },
   { path: '/danh-sach-ho-so/:index/bo-sung-ho-so/:id', name: 'HoanThienBoSungHoSoDetail', component: HoanThienBoSungHoSoDetail, props: true },
