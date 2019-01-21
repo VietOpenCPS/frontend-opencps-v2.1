511b1a85ce90e7ddd08ea0a6ca64ef693d938142
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 18:28:10 +0700

    C

--- dynamic_report/src/components/DetailForward.vue
+++ dynamic_report/src/components/DetailForward.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <tiep-nhan-ho-so-detail ref="tiepnhanhosodetail" :formCode="formCode" ></tiep-nhan-ho-so-detail>
+    13123
   </div>
 </template>
 
@@ -8,16 +8,16 @@
 // import ChuyenPhatKetQuaDetail from './ChuyenPhatKetQuaDetail.vue'
 // import FeeDetail from './FeeDetail.vue'
 // import HoSoLienThongTinhTrangXuLyDetail from './HoSoLienThongTinhTrangXuLyDetail.vue'
-import TiepNhanHoSoDetail from './TiepNhanHoSoDetail.vue'
 // import TiepNhanHoSoTrucTuyenDetail from './TiepNhanHoSoTrucTuyenDetail.vue'
 // import TraKetQuaDetail from './TraKetQuaDetail.vue'
 // import XemChiTietHoSoDetail from './XemChiTietHoSoDetail.vue'
+import XemChiTietHoSoDetailCanBo from './XemChiTietHoSoDetailCanBo.vue'
 
 export default {
   props: ['id', 'formCode'],
   components: {
     // 'ho-so-lien-thong-tinh-trang-xu-ly-detail': HoSoLienThongTinhTrangXuLyDetail,
-    'tiep-nhan-ho-so-detail': TiepNhanHoSoDetail
+    'xem-chi-tiet-ho-so-detail-can-bo': XemChiTietHoSoDetailCanBo
   },
   data: () => ({
     initData: null,
