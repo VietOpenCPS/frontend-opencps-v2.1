de76634fffc8b253054ac19ea04f29647de69681
binhthgc <binhth.vuejx@gmail.com>
2019-01-21 10:00:14 +0700

    c

--- dynamic_report/src/App.vue
+++ dynamic_report/src/App.vue
@@ -1,56 +1,53 @@
 <template>
   <v-app id="app_dynamic_report" class="onegate__fe">
-    <div v-if="trangThaiHoSoList.length !== 0 && viewMobile">
-      <div class="row-header mb-2 py-2" style="background-color: #070f52">
-        <div class="ml-2 text-bold white--text"> <span>QUẢN LÝ HỒ SƠ</span> </div>
-      </div>
-      <div style="max-height:200px;overflow:hidden;overflow-y:scroll">
-        <content-placeholders class="mt-3" v-if="loading">
-          <content-placeholders-text :lines="7" />
-        </content-placeholders>
-        <v-list v-else class="py-0 nav_trang_thai_ho_so">
-          <v-list-group
-            v-for="(item, index) in trangThaiHoSoList"
-            v-model="item.active"
-            :key="index"
-            prepend-icon="description"
-            :append-icon="item.hasOwnProperty('items') ? '' : ''"
-            no-action
-            ref="listGroupCustom"
-          >
-            <v-list-tile slot="activator" class="px-0"
-              @click="toTableIndexing(item, index)" >
-              <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                <span v-if="item.hasOwnProperty('counter') && item['counter'] > -1 && item['menuType'] !== 3" class="status__counter_group status__counter">
-                  {{item.counter}}
-                </span>
-                <span v-else-if="item['menuType'] !== 3" class="status__counter_group status__counter">
-                  <v-progress-circular :width="1" :size="16" indeterminate color="red"></v-progress-circular>
-                </span>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile class="px-0" v-if="item.items && item.items.length > 1" v-for="subItem in item.items" :key="subItem.stepCode"
-              v-on:click.native="filterSteps(subItem)"
-              :class="{'list__tile--active': String(currentStep) === String(subItem.stepCode)}"
-              >
-              <v-list-tile-action>
-                <v-icon color="primary" v-if="String(currentStep) === String(subItem.stepCode)">play_arrow</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>{{ subItem.menuStepName }}</v-list-tile-title>
-                <span v-if="subItem.hasOwnProperty('counter') && subItem['counter'] > -1" class="status__counter">
-                  {{subItem.counter}}
-                </span>
-                <span v-else class="status__counter">
-                  <v-progress-circular :width="1" :size="16" indeterminate color="red"></v-progress-circular>
-                </span>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list-group>
-        </v-list>
-      </div>
-    </div>
+    <v-navigation-drawer app clipped floating width="240"
+      :class='{"detail_state": detailState !== 0}' v-if="trangThaiHoSoList.length !== 0 && !viewMobile"
+    >
+      <content-placeholders class="mt-3" v-if="loading">
+        <content-placeholders-text :lines="7" />
+      </content-placeholders>
+      <v-list v-else class="py-0 nav_trang_thai_ho_so">
+        <v-list-group
+          v-for="(item, index) in trangThaiHoSoList"
+          v-model="item.active"
+          :key="index"
+          prepend-icon="description"
+          :append-icon="item.hasOwnProperty('items') ? '' : ''"
+          no-action
+          ref="listGroupCustom"
+        >
+          <v-list-tile slot="activator"
+            @click="toTableIndexing(item, index)" >
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              <span v-if="item.hasOwnProperty('counter') && item['counter'] > -1 && item['menuType'] !== 3" class="status__counter_group status__counter">
+                {{item.counter}}
+              </span>
+              <span v-else-if="item['menuType'] !== 3" class="status__counter_group status__counter">
+                <v-progress-circular :width="1" :size="16" indeterminate color="red"></v-progress-circular>
+              </span>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="item.items && item.items.length > 1" v-for="subItem in item.items" :key="subItem.stepCode"
+            v-on:click.native="filterSteps(subItem)"
+            :class="{'list__tile--active': String(currentStep) === String(subItem.stepCode)}"
+            >
+            <v-list-tile-action>
+              <v-icon color="primary" v-if="String(currentStep) === String(subItem.stepCode)">play_arrow</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ subItem.menuStepName }}</v-list-tile-title>
+              <span v-if="subItem.hasOwnProperty('counter') && subItem['counter'] > -1" class="status__counter">
+                {{subItem.counter}}
+              </span>
+              <span v-else class="status__counter">
+                <v-progress-circular :width="1" :size="16" indeterminate color="red"></v-progress-circular>
+              </span>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
       <router-view></router-view>
     </v-content>
