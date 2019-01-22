2d3b9e1837d70b5e5d164c7b47451aed097168ca
binhthgc <binhth.vuejx@gmail.com>
2019-01-22 10:30:00 +0700

    c

--- dynamic_report/src/main.js
+++ dynamic_report/src/main.js
@@ -5,19 +5,40 @@ import App from './App.vue'
 import router from './router_dev'
 import { store } from './store'
 import VueContentPlaceholders from 'vue-content-placeholders'
-import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.min.css'
-import 'v-suggestions/dist/v-suggestions.css'
-import 'svg-pan-zoom/dist/svg-pan-zoom.min.js'
 import axios from 'axios'
 
-axios.defaults.headers.common['Token'] = window.Liferay !== undefined ? window.Liferay.authToken : ''
 Vue.use(VueContentPlaceholders)
+
+let groupId = window.themeDisplay !== undefined ? window.themeDisplay.getScopeGroupId() : 0
+
+axios.defaults.withCredentials = true
+axios.defaults.headers.common['Token'] = window.Liferay !== undefined ? window.Liferay.authToken : ''
+axios.defaults.headers.common['groupId'] = groupId
+
 Vue.config.productionTip = true
 
 Vue.mixin({
   methods: {
-    getOriginality: function () {
-      return 3
+    getScopeGroupId: function () {
+      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
+        return window.themeDisplay.getScopeGroupId()
+      } else {
+        return 0
+      }
+    },
+    getAuthToken: function () {
+      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
+        return window.Liferay.authToken
+      } else {
+        return ''
+      }
+    },
+    getUserId: function () {
+      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
+        return window.themeDisplay.getUserId()
+      } else {
+        return 0
+      }
     }
   }
 })
@@ -25,9 +46,18 @@ Vue.mixin({
 new Vue({
   router,
   store,
-  beforeCreate () {
+  created() {
     var vm = this
-    vm.$store.dispatch('loadInitResource')
+    vm.$nextTick(function() {
+      vm.$store.dispatch('loadInitResource')
+      if (window.location.href.endsWith('#/')) {
+        let strQuery = ''
+        if (window.location.href.includes('/so-theo-doi')) {
+          strQuery = '?reportType=register'
+        }
+        vm.$router.push('/bao-cao/0' + strQuery)
+      }
+    })
   },
   render: function (h) { return h(App) }
 }).$mount('#app_dynamic_report')
