bb7359332d0cb3799d1dff41e25d5a627162962e
binhthgc <binhth.alpaca@gmail.com>
2019-01-23 03:57:46 +0700

    c

--- voting_fix/src/App.vue
+++ voting_fix/src/App.vue
@@ -3,18 +3,163 @@
     <v-content>
       <router-view></router-view>
     </v-content>
-    <go-top :size="42" bg-color="#0064c7"></go-top>
   </v-app>
 </template>
 
 <script>
   
-  import GoTop from '@inotom/vue-go-top'
   export default {
     data: () => ({
     }),
     components: {
       GoTop
+    },
+    computed: {
+      currentIndex () {
+        return this.$store.getters.index
+      },
+      govAgencyList () {
+        return this.$store.getters.getAgencyList
+      },
+      domainList () {
+        return this.$store.getters.getDomainList
+      },
+      levelList () {
+        return this.$store.getters.getLevelList
+      },
+      viewMobile () {
+        return isMobile
+      }
+    },
+    created () {
+      var vm = this
+      console.log('isMobile', isMobile)
+      vm.$nextTick(function () {
+        vm.pathRouter = isMobile ? '/m/thu-tuc-hanh-chinh' : '/thu-tuc-hanh-chinh'
+        let current = vm.$router.history.current
+        let newQuery = current.query
+        console.log('current', current)
+        console.log('newQuery', newQuery)
+        vm.$store.dispatch('getGovAgency').then(function (result) {
+          vm.$store.commit('setAgencyList', result)
+          if ((vm.govAgencyList.length > 0 && current.hasOwnProperty('name') && (current.name === 'Landing') && !newQuery.hasOwnProperty('agency')) ||
+          (vm.govAgencyList.length > 0 && current.hasOwnProperty('name') && (current.name === 'NotFound') && !newQuery.hasOwnProperty('agency'))
+          ) {
+            vm.currentAgency = vm.govAgencyList[0].administrationCode
+            let queryString = '?'
+            newQuery['page'] = 1
+            newQuery['agency'] = vm.govAgencyList[0].administrationCode
+            for (let key in newQuery) {
+              if (newQuery[key] !== '' && newQuery[key] !== 'undefined' && newQuery[key] !== undefined && newQuery[key] !== null) {
+                queryString += key + '=' + newQuery[key] + '&'
+              }
+            }
+            vm.$router.push({
+              path: vm.pathRouter + queryString,
+              query: {
+                renew: Math.floor(Math.random() * (100 - 1 + 1)) + 1
+              }
+            })
+          } else {
+            vm.currentAgency = newQuery.hasOwnProperty('agency') ? newQuery.agency : ''
+          }
+        })
+        let filterDomain = {
+          agencyCode: ''
+        }
+        vm.$store.dispatch('getDomain', filterDomain).then(function (result) {
+          vm.$store.commit('setDomainList', result)
+        })
+        vm.$store.dispatch('getLevelList').then(function (result) {
+          vm.$store.commit('setLevelList', result)
+        })
+      })
+    },
+    watch: {
+      '$route': function (newRoute, oldRoute) {
+        let vm = this
+        let currentParams = newRoute.params
+        let currentQuery = newRoute.query
+        if (currentParams.hasOwnProperty('index') && currentParams['index']) {
+          vm.isDetail = true
+        } else {
+          vm.isDetail = false
+        }
+        vm.currentAgency = currentQuery.hasOwnProperty('agency') ? currentQuery.agency : ''
+        vm.currentDomain = currentQuery.hasOwnProperty('domain') ? currentQuery.domain : ''
+        vm.currentLevel = currentQuery.hasOwnProperty('level') ? currentQuery.level : ''
+      }
+    },
+    methods: {
+      filterAgency (item) {
+        var vm = this
+        vm.currentAgency = item.administrationCode
+        let current = vm.$router.history.current
+        let newQuery = current.query
+        let queryString = '?'
+        newQuery['page'] = 1
+        newQuery['agency'] = item.administrationCode
+        newQuery['domain'] = ''
+        newQuery['level'] = ''
+        for (let key in newQuery) {
+          if (newQuery[key] !== '' && newQuery[key] !== 'undefined' && newQuery[key] !== undefined && newQuery[key] !== null) {
+            queryString += key + '=' + newQuery[key] + '&'
+          }
+        }
+        vm.$router.push({
+          path: vm.pathRouter + queryString,
+          query: {
+            renew: Math.floor(Math.random() * (100 - 1 + 1)) + 1
+          }
+        })
+      },
+      filterDomain (item) {
+        var vm = this
+        vm.currentDomain = item.domainCode
+        let current = vm.$router.history.current
+        let newQuery = current.query
+        let queryString = '?'
+        newQuery['page'] = 1
+        newQuery['domain'] = item.domainCode
+        newQuery['agency'] = ''
+        newQuery['level'] = ''
+        for (let key in newQuery) {
+          if (newQuery[key] !== '' && newQuery[key] !== 'undefined' && newQuery[key] !== undefined && newQuery[key] !== null) {
+            queryString += key + '=' + newQuery[key] + '&'
+          }
+        }
+        vm.$router.push({
+          path: vm.pathRouter + queryString,
+          query: {
+            renew: Math.floor(Math.random() * (100 - 1 + 1)) + 1
+          }
+        })
+      },
+      filterLevel (item) {
+        var vm = this
+        vm.currentLevel = item.level
+        let current = vm.$router.history.current
+        let newQuery = current.query
+        let queryString = '?'
+        newQuery['page'] = 1
+        newQuery['domain'] = ''
+        newQuery['agency'] = ''
+        newQuery['level'] = item.level
+        for (let key in newQuery) {
+          if (newQuery[key] !== '' && newQuery[key] !== 'undefined' && newQuery[key] !== undefined && newQuery[key] !== null) {
+            queryString += key + '=' + newQuery[key] + '&'
+          }
+        }
+        vm.$router.push({
+          path: vm.pathRouter + queryString,
+          query: {
+            renew: Math.floor(Math.random() * (100 - 1 + 1)) + 1
+          }
+        })
+      },
+      goBack () {
+        window.history.back()
+      }
     }
   }
 </script>
