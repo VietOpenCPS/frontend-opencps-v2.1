c0720fafef7a022291f584e1f503e5a7068f07a4
binhthgc <binhth.vuejx@gmail.com>
2019-01-22 11:59:01 +0700

    c

--- deliverable/src/App.vue
+++ deliverable/src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <v-app id="app_deliverable">
     <v-navigation-drawer app clipped floating width="240">
-      <div class="drawer__filter">
-        <v-list dense style="padding: 0;" class="report_list">
-          <v-list-tile
-            v-for="(item, indexItem) in itemsReports"
-            :key="indexItem"
-            :to="'/bao-cao/' + indexItem + '?reportType=' + reportTypeFilter"
-          >
-            <v-list-tile-action>
-              <v-icon v-if="String(indexItem) === String(index)" color="blue darken-3">play_arrow</v-icon>
-              <v-icon v-else>description</v-icon>
-            </v-list-tile-action>
+      <v-list dense>
+        <v-list-tile
+          v-for="(item, indexItem) in items"
+          :key="indexItem"
+          :to="'/danh-sach-giay-to/' + indexItem"
+        >
+          <v-list-tile-action>
+            <v-icon v-if="String(indexItem) === String(index)" color="blue darken-3">play_arrow</v-icon>
+            <v-icon v-else>description</v-icon>
+          </v-list-tile-action>
 
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.reportName }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </div>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.typeName }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
     <v-content>
       <router-view></router-view>
@@ -73,74 +71,16 @@
 </template>
 
 <script>
-  import support from './store/support.json'
   export default {
     props: ['index'],
     data: () => ({
       dialog: false,
       drawer: null,
-      dataSocket: {},
-      support: support,
-      itemsReportsConfig: [],
-      itemsGroups: [
-        {
-          value: 'domain',
-          text: 'lĩnh vực'
-        },
-        {
-          value: 'gov',
-          text: 'đơn vị'
-        }
-      ],
-      userConfig: [],
-      reportTypeFilter: ''
+      dataSocket: {}
     }),
     computed: {
-      itemsReports () {
-        return this.$store.getters.itemsReports
-      },
       items () {
         return this.$store.getters.getDeliverableTypes
-      },
-      groupType: {
-        // getter
-        get: function() {
-          return this.$store.getters.groupType
-        },
-        // setter
-        set: function(newValue) {
-          this.$store.commit('setgroupType', newValue)
-        }
-      },
-      reportType: {
-        // getter
-        get: function() {
-          return this.$store.getters.reportType
-        },
-        // setter
-        set: function(newValue) {
-          this.$store.commit('setreportType', newValue)
-        }
-      },
-      selected: {
-        // getter
-        get: function() {
-          return this.$store.getters.selected
-        },
-        // setter
-        set: function(newValue) {
-          this.$store.commit('setselected', newValue)
-        }
-      },
-      selectedText: {
-        // getter
-        get: function() {
-          return this.$store.getters.selectedText
-        },
-        // setter
-        set: function(newValue) {
-          this.$store.commit('setselectedText', newValue)
-        }
       },
       snackbarerror: {
         // getter
@@ -163,52 +103,6 @@
         }
       }
     },
-    created () {
-    var vm = this
-      vm.$nextTick(function () {
-        if (vm.$route.query.reportType !== undefined && vm.$route.query.reportType !== null) {
-          vm.reportTypeFilter = vm.$route.query.reportType
-        }
-        vm.$store.dispatch('getDynamicReports', vm.reportTypeFilter).then(function (result) {
-          vm.itemsReportsConfig = []
-          vm.userConfig = []
-          if (String(vm.index) !== '0') {
-            for (let key in vm.itemsReports) {
-              if (vm.itemsReports[key]['code'] === String(vm.index)) {
-                vm.reportType = vm.itemsReports[key]['document']
-                vm.itemsReportsConfig = vm.itemsReports[key]['filterConfig']['reportConfig']
-                if (vm.itemsReports[key]['userConfig'] !== '') {
-                  let userConfigObjec = vm.itemsReports[key]['userConfig']
-                  if (userConfigObjec.hasOwnProperty(vm.getUserId())) {
-                    vm.userConfig = userConfigObjec[vm.getUserId()]
-                  }
-                }
-                break
-              }
-            }
-          } else {
-            vm.reportType = vm.itemsReports[0]['document']
-            vm.itemsReportsConfig = vm.itemsReports[0]['filterConfig']['reportConfig']
-            if (vm.itemsReports[0]['userConfig'] !== '') {
-              let userConfigObjec = vm.itemsReports[0]['userConfig']
-              if (userConfigObjec.hasOwnProperty(vm.getUserId())) {
-                vm.userConfig = userConfigObjec[vm.getUserId()]
-              }
-            }
-          }
-          vm.selected = []
-          if (vm.userConfig.length > 0) {
-            vm.selected = vm.userConfig
-          } else {
-            for (let keySelected in vm.itemsReportsConfig) {
-              if (vm.itemsReportsConfig[keySelected]['selected']) {
-                vm.selected.push(vm.itemsReportsConfig[keySelected]['value'])
-              }
-            }
-          }
-        })
-      })
-    },
     methods: {
       redirectFilter(val) {
         this.$router.push(val + '?state_change=' + Math.floor(Math.random() * (100 - 1 + 1)) + 1)
@@ -221,42 +115,6 @@
       },
       deliverableRouter (item) {
         console.log(item)
-      },
-      resetConfig () {
-        let vm = this
-        vm.selected = []
-        for (let keySelected in vm.itemsReportsConfig) {
-          if (vm.itemsReportsConfig[keySelected]['selected']) {
-            vm.selected.push(vm.itemsReportsConfig[keySelected]['value'])
-          }
-        }
-      },
-      changeReportType (data) {
-        let vm = this
-        vm.itemsReportsConfig = []
-        for (let key in vm.itemsReports) {
-          if (vm.itemsReports[key]['document'] === data) {
-            vm.itemsReportsConfig = vm.itemsReports[key]['filterConfig']['reportConfig']
-            if (vm.itemsReports[key]['userConfig'] !== '') {
-              let userConfigObjec = vm.itemsReports[key]['userConfig']
-              if (userConfigObjec.hasOwnProperty(vm.getUserId())) {
-                vm.userConfig = userConfigObjec[vm.getUserId()]
-              }
-            }
-            vm.selected = []
-            if (vm.userConfig.length > 0) {
-              vm.selected = vm.userConfig
-            } else {
-              for (let keySelected in vm.itemsReportsConfig) {
-                if (vm.itemsReportsConfig[keySelected]['selected']) {
-                  vm.selected.push(vm.itemsReportsConfig[keySelected]['value'])
-                }
-              }
-            }
-            vm.$router.push('/bao-cao/' + vm.itemsReports[key]['code'])
-            break
-          }
-        }
       }
     }
   }
