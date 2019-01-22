7e627290a19c678a2e00538c3d71d1aa8b748f49
binhthgc <binhth.alpaca@gmail.com>
2019-01-23 05:17:37 +0700

    c

--- user_profile/src/router_dev/index.js
+++ user_profile/src/router_dev/index.js
@@ -5,6 +5,42 @@ import LandingUserProfile from '@/components/screens/Landing'
 
 const routes = [
   { path: '/', component: LandingUserProfile, props: true },
+  {
+    path: '/danh-sach-ho-so/:index',
+    name: 'Landing',
+    component: () => import(/* webpackChunkName: "Landing" */ '@/components/blacklist/Landing.vue'),
+    props: true,
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: '/danh-sach-ho-so/:index/tiep-nhan-ho-so/:id/:formCode',
+    name: 'TiepNhanHoSoDetail',
+    component: () => import(/* webpackChunkName: "TiepNhanHoSoDetail" */ '@/components/blacklist/TiepNhanHoSoDetail.vue'),
+    props: true,
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: '/danh-sach-ho-so/:index/bo-sung-ho-so/:id',
+    name: 'HoanThienBoSungHoSoDetail',
+    component: () => import(/* webpackChunkName: "HoanThienBoSungHoSoDetail" */ '@/components/blacklist/HoanThienBoSungHoSoDetail.vue'),
+    props: true,
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: '/danh-sach-ho-so/:index/ho-so/:id/:formCode',
+    name: 'DetailForward',
+    component: () => import(/* webpackChunkName: "DetailForward" */ '@/components/blacklist/DetailForward.vue'),
+    props: true,
+    meta: {
+      requiresAuth: false
+    }
+  },
   { path: '*', name: 'NotFound', component: NotFound }
 ]
 
