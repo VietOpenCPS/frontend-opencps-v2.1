9cc35e141393d79dfac4c3b4f0bae8f26e7192ac
binhthgc <binhth.alpaca@gmail.com>
2019-01-23 05:04:33 +0700

    c

--- register/src/router_dev/index.js
+++ register/src/router_dev/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Landing from '@/components/blacklist/Landing'
-import TiepNhanHoSoDetail from '@/components/blacklist/TiepNhanHoSoDetail'
-import HoanThienBoSungHoSoDetail from '@/components/blacklist/HoanThienBoSungHoSoDetail'
-import DetailForward from '@/components/blacklist/DetailForward'
 import NotFound from '@/components/NotFound'
 import LandingRegister from '@/components/screens/Landing'
 import XacThucTaiKhoan from '@/components/screens/XacThucTaiKhoan'
@@ -17,10 +13,6 @@ const routes = [
   { path: '/cap-lai-mat-khau', name: 'ForgotPassword', component: ForgotPassword },
   { path: '/xac-thuc-cap-lai-mat-khau', name: 'ConfirmPassword', component: ConfirmPassword },
   { path: '/login', name: 'Login', component: Login },
-  { path: '/danh-sach-ho-so/:index', name: 'Landing', component: Landing, props: true },
-  { path: '/danh-sach-ho-so/:index/tiep-nhan-ho-so/:id/:formCode', name: 'TiepNhanHoSoDetail', component: TiepNhanHoSoDetail, props: true },
-  { path: '/danh-sach-ho-so/:index/bo-sung-ho-so/:id', name: 'HoanThienBoSungHoSoDetail', component: HoanThienBoSungHoSoDetail, props: true },
-  { path: '/danh-sach-ho-so/:index/ho-so/:id/:formCode', name: 'DetailForward', component: DetailForward, props: true },
   { path: '*', name: 'NotFound', component: NotFound }
 ]
 
