663bd4dc2ca7016b71dd14fa8647a61ebde93def
binhthgc <binhth.alpaca@gmail.com>
2019-01-23 05:06:29 +0700

    c

--- voting/src/App.vue
+++ voting/src/App.vue
@@ -3,6 +3,7 @@
     <v-navigation-drawer app clipped floating width="310" v-if="false">
     </v-navigation-drawer>
     <v-content>
+      123123
       <router-view></router-view>
     </v-content>
   </v-app>
