69c32ab5c104be086079b2efe7c1ded0464dcd83
binhthgc <binhth.alpaca@gmail.com>
2019-01-23 01:15:50 +0700

    c

---
+++ survey/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app id="app_survey">
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    })
+  }
+</script>
