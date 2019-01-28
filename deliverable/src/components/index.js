1ababd6af77822fdaf8a9bdf36d8f5b076d98d3e
binhthgc <binhth.vuejx@gmail.com>
2019-01-28 10:16:57 +0700

    c

--- deliverable/src/components/index.js
+++ deliverable/src/components/index.js
@@ -2,7 +2,7 @@ import BbatTableEditorComponent from './ext/BbatTableEditorComponent.vue'
 import BbatTableEditorComponentSimple from './ext/BbatTableEditorComponentSimple.vue'
 import TinyPagination from './ext/TinyPagination.vue'
 import DatetimePicker from './ext/DatetimePicker.vue'
-import ViewPdf from './ext/ViewPdf.vue'
+import ViewPdf from './ext/ViewPDF.vue'
 import ViewLogs from './ext/ViewLogs.vue'
 import AttachedFileTemplate from './ext/AttachedFileTemplate.vue'
 
