e04711f19e2a74b6cc63cbbd1c02669fa587845e
dungdv <sdgaw102@gmail.com>
2020-09-11 17:28:36 +0700

    day code

--- tracuu_hoso/src/components/screens/ThanhToanThanhCong.vue
+++ tracuu_hoso/src/components/screens/ThanhToanThanhCong.vue
@@ -161,8 +161,9 @@ export default {
           secure_hash: query.hasOwnProperty('secure_hash') ? query.secure_hash : '',
           dossierId: query.hasOwnProperty('dossierId') ? query.dossierId : '',
         }
-        vm.$store.dispatch('checkkeypay',filterCheckEpay).then(resCheckEpay => {
-          if (responseCode === '00' && resCheckEpay && resCheckEpay.result) {
+        // vm.$store.dispatch('checkkeypay',filterCheckEpay).then(resCheckEpay => {
+          // if (responseCode === '00' && resCheckEpay && resCheckEpay.result) {
+          if (responseCode === '00') {
             vm.statusDeal = true
           } else {
             vm.statusDeal = false
@@ -224,9 +225,9 @@ export default {
               })
             }
           })
-        }).catch(()=>{
-          vm.statusDeal = false
-        })
+        // }).catch(()=>{
+        //   vm.statusDeal = false
+        // })
 
       }
     })
