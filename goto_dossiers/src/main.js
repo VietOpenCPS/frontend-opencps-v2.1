3f9906c0467bcdd6692317b62bcf8c8e85fcd983
congtrinh0209 <congtrinh0209@gmail.com>
2021-03-05 09:29:00 +0700

    add module chuyển hồ sơ về hoàn thành

---
+++ goto_dossiers/src/main.js
@@ -0,0 +1,58 @@
+import 'idempotent-babel-polyfill'
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import router from './router_dev'
+import { store } from './store'
+import VueContentPlaceholders from 'vue-content-placeholders'
+import axios from 'axios'
+Vue.use(VueContentPlaceholders)
+
+let groupId = window.themeDisplay !== undefined ? window.themeDisplay.getScopeGroupId() : 0
+
+axios.defaults.withCredentials = true
+axios.defaults.headers.common['Token'] = window.Liferay !== undefined ? window.Liferay.authToken : ''
+axios.defaults.headers.common['groupId'] = groupId
+
+Vue.config.productionTip = true
+
+Vue.mixin({
+  data: () => ({
+    isOnegate: false
+  }),
+  methods: {
+    getScopeGroupId: function () {
+      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
+        return window.themeDisplay.getScopeGroupId()
+      } else {
+        return 0
+      }
+    },
+    getAuthToken: function () {
+      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
+        return window.Liferay.authToken
+      } else {
+        return ''
+      }
+    },
+    getUserId: function () {
+      if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
+        return window.themeDisplay.getUserId()
+      } else {
+        return 0
+      }
+    }
+  }
+})
+
+new Vue({
+  router,
+  store,
+  created() {
+    var vm = this
+    vm.$nextTick(function() {
+      
+    })
+  },
+  render: function (h) { return h(App) }
+}).$mount('#goto_dossiers')
