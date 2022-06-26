6702fd5cdb01937405296aa11ba3e27f1af011cb
congtrinh0209 <congtrinh0209@gmail.com>
2022-06-26 17:33:57 +0700

    create voting canbo

--- voting_canbo/src/App.vue
+++ voting_canbo/src/App.vue
@@ -4,18 +4,24 @@
     </v-navigation-drawer>
     <v-content>
       <div class="wrap-login">
+        <div class="wrap-btn-screen">
+          <v-btn class="btn-screen px-0" @click.stop="changeScreen" flat icon color="#940404">
+            <v-icon size="28" v-if="!fullScreen">fullscreen</v-icon>
+            <v-icon size="28" v-else>fullscreen_exit</v-icon>
+          </v-btn>
+        </div>
         <div id="header-app" >
-          <header id="banner">
+          <header id="banner-voting">
             <div class="container layout wrap" style=""> 
-              <div class="flex layout wrap sm6 md5 lg4" style="padding-top: 5px;">
-                <img class="logo-banner" src="http://119.17.200.69:8174/o/hau-giang-theme/images/quoc_huy.svg">
+              <div class="flex layout wrap sm6 md5 lg4" style="padding-top: 10px;">
+                <img class="logo-banner" src="/o/hau-giang-theme/images/quoc_huy.svg">
                 <div  class="flex">
                   <div class="title-banner">TRUNG TÂM HÀNH CHÍNH CÔNG</div>
                   <div class="title-banner">TỈNH HẬU GIANG</div>
                 </div>
               </div>
               <div class="flex sm6 md7 lg8" style="align-self: center;">
-                <div class="title-banner" style="font-size: 20px; text-align: center;">
+                <div class="title-banner-2" style="">
                   <span>QUẦY cảnh sát hành chính </span>
                 </div>
               </div>
@@ -28,11 +34,12 @@
           class="px-0 pt-0"
           tag="section"
         >
+          
           <div class="layout wrap">
             <div class="flex col-left sm6 md5 lg4">
-              <img src="" alt="">
+              <img :src="employeeAvatar" alt="">
               <div class="name-emp">
-                NGUYỄN THỊ TUYẾT MAI
+                {{employeeInfo.employeeFullName ? employeeInfo.employeeFullName : employeeInfo.userName}}
               </div>
             </div>
             <div class="flex col-right sm6 md7 lg8">
@@ -44,7 +51,7 @@
                   <v-btn class="my-0 white--text btn-vote" color="#940404"
                     :loading="loading"
                     :disabled="loading"
-                    @click=""
+                    @click.stop="submitVoting({name: 'Rất hài lòng', value: 3})"
                   >
                     <span>RẤT HÀI LÒNG</span>
                   </v-btn>
@@ -53,7 +60,7 @@
                   <v-btn class="my-0 white--text btn-vote" color="#940404"
                     :loading="loading"
                     :disabled="loading"
-                    @click=""
+                    @click.stop="submitVoting({name: 'Hài lòng', value: 2})"
                   >
                     <span>HÀI LÒNG</span>
                   </v-btn>
@@ -62,7 +69,7 @@
                   <v-btn class="my-0 white--text btn-vote" color="#940404"
                     :loading="loading"
                     :disabled="loading"
-                    @click=""
+                    @click.stop="submitVoting({name: 'Không hài lòng', value: 1})"
                   >
                     <span>KHÔNG HÀI LÒNG</span>
                   </v-btn>
@@ -72,32 +79,63 @@
           </div> 
           
         </v-container>
+        <v-dialog v-model="dialogSuccess" persistent max-width="700px">
+          <v-card>
+            <v-toolbar flat dark color="#940404">
+              <v-toolbar-title style="width: 100%;text-transform: uppercase;">Đánh giá cán bộ thành công</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <p class="text-success" style="text-align: justify">- Cảm ơn quý khách đã tham gia đánh giá cán bộ.</p>
+              <p class="text-success" style="text-align: justify">- Kết quả đánh giá của bạn đã giúp chúng tôi cải thiện, nâng cao chất lượng phục vụ và cung ứng
+                dịch vụ giải quyết thủ tục hành chính cho người dân, tổ chức, doanh nghiệp.
+              </p>
+              <p class="my-2 text-success" style="text-align:center">(Có thể đánh giá sau {{countDown}} giây)</p>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
       </div>
     </v-content>
   </v-app>
 </template>
 
 <script>
-  import toastr from 'toastr'
   import axios from 'axios'
   export default {
     data: () => ({
+      dialogSuccess: false,
+      fullScreen: false,
       loading: false,
-      isMobile: false
+      isMobile: false,
+      employeeInfo: '',
+      employeeAvatar: '/o/hau-giang-theme/images/avatar-default.png',
+      countDown: 0
     }),
     beforeDestroy () {
       if (typeof window !== 'undefined') {
         window.removeEventListener('resize', this.onResize, { passive: true })
       }
     },
-    mounted () {
-      this.onResize()
-      window.addEventListener('resize', this.onResize, { passive: true })
-      if (this.isMobile) {
-        $('section#content').css('padding-left', '0px')
-      }
+    mounted: function () {
+      let vm = this
+      this.$nextTick(function () {
+        // let userId = window.themeDisplay ? window.themeDisplay.getUserId() : ''
+        // if (userId && userId != '20139') {
+        //   vm.fullScreen = true
+        //   setTimeout(function () {
+        //     vm.openFullscreen()
+        //   }, 500)
+        // }
+      })
     },
     computed: {
+    },
+    created () {
+      let vm = this
+      vm.getEmployee()
+      setInterval(function () {
+        // location.reload()
+        vm.getEmployee()
+      }, 1*60*1000)
     },
     methods: {
       onResize () {
@@ -105,86 +143,135 @@
         let isMobile = window.innerWidth < 1024
         vm.isMobile = isMobile
       },
-      loadEmployees ({commit, state}, data) {
-        return new Promise((resolve, reject) => {
-          commit('setLoading', true)
-          store.dispatch('loadInitResource').then(function (result1) {
-            let param = {
-              headers: {
-                groupId: state.initData.groupId
+      getEmployee () {
+        let vm = this
+        let param = {
+          headers: {
+            groupId: window.themeDisplay.getScopeGroupId()
+          }
+        }
+        let userId = window.themeDisplay.getUserId()
+        if (userId) {
+          axios.get('/o/v1/opencps/users/' + userId, param).then(function (response) {
+            vm.employeeInfo = response.data
+            axios.get('/o/v1/opencps/users/avatar/' + vm.employeeInfo['className'] + '/' + vm.employeeInfo['classPK'], param).then(function (response) {
+              let seriable = response.data
+              let portalURL = ''
+              if (window.themeDisplay !== null && window.themeDisplay !== undefined) {
+                portalURL = window.themeDisplay.getPortalURL()
               }
-            }
-            axios.get(support.initData.employeeApi + '/' + data.itemCode, param).then(result => {
-              if (result.data) {
-                resolve(result.data.data)
+              if (seriable) {
+                vm.employeeAvatar = portalURL + seriable
               } else {
-                resolve([])
+                vm.employeeAvatar = '/o/hau-giang-theme/images/avatar-default.png'
               }
-            }).catch(xhr => {
-              reject(xhr)
+            }).catch(function (xhr) {
             })
           })
+        }
+      },
+      submitVoting (vote) {
+        let vm = this
+        let data = JSON.stringify({
+          "dossierNo": "",
+          "govAgencyCode": "",
+          "govAgencyName": "",
+          "employeeEmail": vm.employeeInfo ? vm.employeeInfo.employeeEmail : '',
+          "employeeName": vm.employeeInfo ? vm.employeeInfo.employeeFullName : '',
+          "votingName": vote.name,
+          "votingValue": vote.value,
+          "groupId": window.themeDisplay ? window.themeDisplay.getScopeGroupId() : ''
+        })
+        vm.loading = true
+        let config = {
+          method: 'post',
+          url: '/o/rest/v2/votings/rateEmployee',
+          headers: { 
+            'Content-Type': 'application/json'
+          },
+          data : data
+        }
+
+        axios(config)
+        .then(function (response) {
+          vm.loading = false
+          vm.dialogSuccess = true
+          vm.countDown = 30
+          var downloadTimer = setInterval(function(){
+            if(vm.countDown <= 0){
+              clearInterval(downloadTimer)
+            }
+            vm.countDown -= 1
+          }, 1000)
+          setTimeout (function () {
+            vm.dialogSuccess = false
+          }, 30000)
+        })
+        .catch(function (error) {
+          vm.loading = false
         })
       },
-      getEmployee ({commit, state}, data) {
-        return new Promise((resolve, reject) => {
-          store.dispatch('loadInitResource').then(function (result1) {
-            let param = {
-              headers: {
-                groupId: state.initData.groupId
-              }
-            }
-            axios.get(support.initData.detailEmployeeApi + '/' + data.employeeId, param).then(result => {
-              resolve(result.data)
-            }).catch(xhr => {
-              reject(xhr)
-            })
-          })
-        })
+      changeScreen () {
+        let vm = this
+        if (vm.fullScreen) {
+          vm.closeFullscreen()
+          vm.fullScreen = false
+        } else {
+          vm.openFullscreen()
+          vm.fullScreen = true
+        }
       },
-      submitVoting ({commit, state}, data) {
-        return new Promise((resolve, reject) => {
-          store.dispatch('loadInitResource').then(function (result1) {
-            let params = new URLSearchParams()
-            const config = {
-              headers: {
-                'groupId': state.initData.groupId
-              }
-            }
-            if (data.comment) {
-              params.append('comment', data.comment)
-            }
-            params.append('selected', data.selected)
-            params.append('className', data.className)
-            params.append('classPk', data.classPk)
-            axios.post(state.endPointApi + '/postal/votings/' + data.votingId + '/results', params, config).then(result => {
-              resolve(result.data)
-            }).catch(xhr => {
-              reject(xhr)
-            })
-          })
-        })
+      openFullscreen () {
+        var elem = document.documentElement;
+        if (elem.requestFullscreen) {
+          elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+          elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+          elem.msRequestFullscreen();
+        }
+      },
+      closeFullscreen() {
+        var elem = document.documentElement;
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+          document.msExitFullscreen();
+        }
       }
     }
   }
 </script>
 
 <style>
+  .wrap-btn-screen {
+    position: absolute;
+    right: 10px;
+    top: 78px;
+  }
+  .btn-screen {
+    
+  }
+  header#banner, nav#navigation, footer#footer, .yhy-append-wrap {
+    display: none !important;
+  }
   #app {
     background: transparent !important
   }
   #header-app {
-    height: 64px;
+    height: 72px;
     width: 100%;
     background: transparent;
     box-shadow: 0px 4px 4px rgb(0 0 0 / 25%);
     padding: 0;
   }
-  #banner .container {
-    height: 64px;
+  #banner-voting .container {
+    height: 72px;
     padding: 0;
   }
-  #banner .container a {
+  #banner-voting .container a {
     text-decoration: none;
     display: flex;
     text-align: center;
@@ -205,6 +292,16 @@
     color: #940404;
     text-transform: uppercase;
   }
+  .title-banner-2 {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: bold;
+    line-height: 26px;
+    color: #940404;
+    text-transform: uppercase;
+    font-size: 20px;
+    text-align: center;
+  }
   #wrap-content {
     padding-top: 50px !important;
   }
@@ -223,8 +320,8 @@
   .btn-vote {
     width: 300px;
     margin-bottom: 20px !important;
-    border-radius: 10px;
-    font-size: 22px;
+    border-radius: 10px !important;
+    font-size: 22px !important;
     height: 64px !important;
     box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 5px 0px rgb(0 0 0 / 20%), 0px 5px 5px 0px rgb(0 0 0 / 22%) !important;
   }
@@ -236,19 +333,20 @@
     background: #940404!important;
     padding: 10px;
     border-radius: 15px;
-    max-width: 700px;
+    max-width: 600px;
     margin: 0 auto;
     margin-bottom: 30px;
   }
   .col-left img {
-    width: 300px;
-    height: 300px;
+    width: 250px;
+    height: 330px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   }
   .col-left .name-emp {
-    width: 300px;
+    max-width: 300px;
     margin: 0 auto;
-    margin-top: 10px;
-    font-size: 24px;
+    margin-top: 15px;
+    font-size: 22px;
     font-weight: 400;
     color: #fff;
     background: #940404!important;
@@ -256,7 +354,7 @@
     border-radius: 10px;
   }
   .wrap-login {
-    background: url(https://quantritaikhoan.haugiang.gov.vn/images/bg-login.png) no-repeat center;
+    background: url(/o/hau-giang-theme/images/bg-trongdong.png) no-repeat center;
     background-size: cover;
     height: 100vh;
     width: 100%;
@@ -313,11 +411,27 @@
     margin-bottom: 15px;
     text-transform: uppercase;
   }
+  .text-success {
+    font-size: 18px;
+    font-weight: 500;
+    color: #000000de;
+  }
   .input-text .v-icon{
     color: #B11312 !important;
     font-size: 20px !important;
   }
   .input-text  .v-input__slot {
     box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 8%), 0px 2px 2px 0px rgb(0 0 0 / 0%), 0px 1px 5px 0px rgb(0 0 0 / 7%) !important;
+  }
+  @media (max-width: 767px) {
+    #app_voting_canbo .title-banner {
+      font-size: 16px !important;
+    }
+    #app_voting_canbo .title-banner-2 {
+      margin-top: 20px !important ;
+    }
+    .btn-vote {
+      width: 280px;
+    }
   }
 </style>
